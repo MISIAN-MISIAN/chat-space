a98c70f08db57af11708206cff2b490a7eb22b75
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2019-11-06 12:40:01 +0900

    ユーザー管理画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
-  root to: "messages#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  devise_for :users
+  root "messages#index"
+  resources :users, only: [:edit, :update]
+end 

a53258141370fa5678f3cb2d67ac6b6169e9a868
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2019-11-08 23:48:36 +0900

    メッセージ送信機能修正

--- db/schema.rb
+++ db/schema.rb
@@ -29,11 +29,11 @@ ActiveRecord::Schema.define(version: 20191106121008) do
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
-    t.string   "image"
+    t.text     "image",      limit: 65535
     t.integer  "group_id"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
     t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
